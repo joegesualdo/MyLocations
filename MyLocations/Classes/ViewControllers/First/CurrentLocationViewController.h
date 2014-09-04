22d3178e54f9d33da126b10876cebfb39ec8d86a
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-04 16:12:01 -0400

    Layout CurrentLocationView

--- MyLocations/Classes/ViewControllers/First/CurrentLocationViewController.h
+++ MyLocations/Classes/ViewControllers/First/CurrentLocationViewController.h
@@ -10,4 +10,13 @@
 
 @interface CurrentLocationViewController : UIViewController
 
+@property(nonatomic, weak) IBOutlet UILabel *messageLabel;
+@property(nonatomic, weak) IBOutlet UILabel *latitudeLabel;
+@property(nonatomic, weak) IBOutlet UILabel *longitudeLabel;
+@property(nonatomic, weak) IBOutlet UILabel *addressLabel;
+@property(nonatomic, weak) IBOutlet UIButton *tagButton;
+@property(nonatomic, weak) IBOutlet UIButton *getButton;
+
+- (IBAction)getLocation:(id)sender;
+
 @end
