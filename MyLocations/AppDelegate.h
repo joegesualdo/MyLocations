81ff004bfc98455f7a20a893bfcf2f8c809cb62d
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-04 15:22:31 -0400

    Initial Commit

---
+++ MyLocations/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyLocations
+//
+//  Created by Joe Gesualdo on 9/4/14.
+//  Copyright (c) 2014 Joe Gesualdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyLocations/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  MyLocations
+//
+//  Created by Joe Gesualdo on 9/4/14.
+//  Copyright (c) 2014 Joe Gesualdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
