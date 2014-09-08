45129dc9febe471706b0f816c4e14ba25f05d04e
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-08 14:44:14 -0400

    Create a xcdatamodel file for CoreData; create a location entity and a location NSManagedObject to represent that entity

---
+++ MyLocations/Classes/Models/Location.h
@@ -0,0 +1,25 @@
+//
+//  Location.h
+//  MyLocations
+//
+//  Created by Joe Gesualdo on 9/8/14.
+//  Copyright (c) 2014 Joe Gesualdo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+// the Location class extends NSManagedObject instead of the regular NSObject, because this represents a CoreData entity
+@interface Location : NSManagedObject
+
+// Even though you chose the datatype Double for latitude and longitude, these properties are listed as NSNumber objects instead of double values. Core Data stores everything as objects, not as primitive values. Anything that you would normally use an int, float, double or BOOL for will need to become an NSNumber in Core Data.
+@property (nonatomic, retain) NSNumber * latitude;
+@property (nonatomic, retain) NSNumber * logitude;
+@property (nonatomic, retain) NSString * locationDescription;
+// Because you made placemark a Transformable property, Xcode doesn’t really know what kind of object this will be, so it chose the generic datatype id. You know it’s going to be a CLPlacemark object, so you can make things easier for yourself by changing it. It used to be this:
+// @property (nonatomic, retain) id placemark;
+@property (nonatomic, retain) CLPlacemark *placemark;
+@property (nonatomic, retain) NSString * category;
+
+@end
---
+++ MyLocations/Classes/Models/Location.m
@@ -0,0 +1,21 @@
+//
+//  Location.m
+//  MyLocations
+//
+//  Created by Joe Gesualdo on 9/8/14.
+//  Copyright (c) 2014 Joe Gesualdo. All rights reserved.
+//
+
+#import "Location.h"
+
+
+@implementation Location
+
+// The @dynamic keyword tells the compiler that these properties will be resolved at runtime by Core Data. When you put a new value into one of these properties, Core Data will put that value into the data store for safekeeping, instead of in an instance variable. That’s all there is to it.
+@dynamic latitude;
+@dynamic logitude;
+@dynamic locationDescription;
+@dynamic placemark;
+@dynamic category;
+
+@end
