d7f6f8aa27be49750397cfab662b7cb20e37876c
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-10 13:35:31 -0400

    Customize style for location details view

--- MyLocations/Classes/ViewControllers/LocationDetails/LocationDetailsViewController.m
+++ MyLocations/Classes/ViewControllers/LocationDetails/LocationDetailsViewController.m
@@ -85,6 +85,16 @@
       }
     }
   }
+  
+  //  It changes the colors of the table view (but not the cells) and some of the other controls.
+  self.tableView.backgroundColor = [UIColor blackColor];
+  self.tableView.separatorColor = [UIColor colorWithWhite:1.0f alpha:0.2f];
+  self.descriptionTextView.textColor = [UIColor whiteColor];
+  self.descriptionTextView.backgroundColor = [UIColor blackColor];
+  self.photoLabel.textColor = [UIColor whiteColor];
+  self.photoLabel.highlightedTextColor = self.photoLabel.textColor;
+  self.addressLabel.textColor = [UIColor colorWithWhite:1.0f alpha:0.4f];
+  self.addressLabel.highlightedTextColor = self.addressLabel.textColor;
 
   
   // put the context of the UITextView into the descriptionText variable
@@ -211,6 +221,26 @@
   } else {
     return 44;
   }
+}
+
+
+// The willDisplayCell delegate method is called just before a cell becomes visible. Here you can do some last-minute customizations on the cell and its contents.
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell
+                                        forRowAtIndexPath:(NSIndexPath *)indexPath {
+  cell.backgroundColor = [UIColor blackColor];
+  cell.textLabel.textColor = [UIColor whiteColor];
+  cell.textLabel.highlightedTextColor = cell.textLabel.textColor;
+  cell.detailTextLabel.textColor = [UIColor colorWithWhite:1.0f alpha:0.4f];
+  cell.detailTextLabel.highlightedTextColor = cell.detailTextLabel.textColor;
+  UIView *selectionView = [[UIView alloc] initWithFrame:CGRectZero];
+  selectionView.backgroundColor = [UIColor colorWithWhite:1.0f alpha:0.2f];
+  cell.selectedBackgroundView = selectionView;
+  
+if (indexPath.row == 2) {
+  UILabel *addressLabel = (UILabel *)[cell viewWithTag:100];
+  addressLabel.textColor = [UIColor whiteColor];
+  addressLabel.highlightedTextColor = addressLabel.textColor;
+}
 }
 
 #pragma mark - IBActions
